bfc09f432b952583050ee6e475023b15c1379ea9
Nooc <nooc@nooc.ink>
2021-10-10 16:12:35 +0800

    Complete android implementation

--- android/src/main/java/com/mufan/capacitor/plugin/alipay/AlipayPlugin.java
+++ android/src/main/java/com/mufan/capacitor/plugin/alipay/AlipayPlugin.java
@@ -1,22 +1,44 @@
 package com.mufan.capacitor.plugin.alipay;
 
+import android.app.Activity;
+import android.os.Message;
+
+import com.alipay.sdk.app.PayTask;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import java.util.Map;
+
 @CapacitorPlugin(name = "Alipay")
 public class AlipayPlugin extends Plugin {
-
-    private Alipay implementation = new Alipay();
+    private Alipay alipay = new Alipay();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void request(PluginCall call) {
+        Alipay.lastCall = call;
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        final String orderInfo = call.getString("orderInfo");
+        final Activity act = getBridge().getActivity();
+
+        final Runnable payRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                PayTask task = new PayTask(act);
+                Map<String, String> result = task.payV2(orderInfo, true);
+
+                Message msg = new Message();
+                msg.what = alipay.SDK_PAY_FLAG;
+                msg.obj = result;
+
+                alipay.mHandler.sendMessage(msg);
+            }
+        };
+
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
     }
 }
